df0226818a8b3b7188702fc7572105fe8eaea8da
teitei-tk <teitei.tk@gmail.com>
2018-01-10 22:55:09 +0900

    add react-router module

--- src/components/Content/content.tsx
+++ src/components/Content/content.tsx
@@ -1,14 +1,15 @@
 import * as React from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 import Authenticator from "../../lib/authenticator";
 
 export default class extends React.Component {
   render() {
     return (
-      <div>
+      <Router>
         <h1>Looks Good To Your</h1>
         <Authenticator />
-      </div>
+      </Router>
     );
   }
 }
